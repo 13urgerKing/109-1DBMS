57b3c86c91d3adfdea35d9e9cf75a37b756a0a67
Larry_NTUT_LAB1321 <61963076+NTUT-Larry@users.noreply.github.com>
2020-12-27 22:13:42 +0800

    add query commands

--- Query/Create DB and Tables.sql
+++ Query/Create DB and Tables.sql
@@ -15,7 +15,6 @@ CREATE TABLE Seller (
   User_No varchar(5) NOT NULL,
   Bank_account varchar(15) NOT NULL,
   PRIMARY KEY(User_No)
-  /*FOREIGN KEY (User_No) REFERENCES User(User_No)*/
 );
 
 CREATE TABLE Buyer (
@@ -23,55 +22,48 @@ CREATE TABLE Buyer (
   Member_Lvl INT NOT NULL,
   Wallet INT NOT NULL,
   PRIMARY KEY(User_No)
-  /*FOREIGN KEY (User_No) REFERENCES User(User_No)*/
 );
 
 CREATE TABLE Coupon (
   Coupon_No varchar(5) NOT NULL,
-  User_No varchar(5) NOT NULL,
-  Type INT NOT NULL,
+  Buyer_No varchar(5) NOT NULL,
   Amount INT NOT NULL,
+  Used Boolean NOT NULL,
   Expiry_date DATE NOT NULL,
   PRIMARY KEY(Coupon_No)
-  /*FOREIGN KEY (User_No) REFERENCES User(User_No)*/
 );
 
 CREATE TABLE Game (
   Game_No varchar(5) NOT NULL,
-  User_No varchar(5) NOT NULL,
+  Seller_No varchar(5) NOT NULL,
   Price INT NOT NULL,
   Sales_volume INT NOT NULL,
   Category varchar(10) NOT NULL,
   Name varchar(50) NOT NULL,
   Description varchar(500) NOT NULL,
-  ImageURL varchar(20) NOT NULL,
+  ImageURL varchar(50) NOT NULL,
+  Available Boolean NOT NULL,
   PRIMARY KEY(Game_No)
-  /*FOREIGN KEY (User_No) REFERENCES User(User_No)*/
 );
 
 CREATE TABLE Order_info (
-  User_No varchar(5) NOT NULL,
   Order_No varchar(5) NOT NULL,
-  Coupon_No varchar(5) NOT NULL,
+  Buyer_No varchar(5) NOT NULL,
+  Coupon_No varchar(5) NULL,
   Date DATE NOT NULL,
   Price INT NOT NULL,
   PRIMARY KEY(Order_No)
-  /*FOREIGN KEY (Game_No) REFERENCES Game(Game_No)*/
-  /*FOREIGN KEY (Coupon_No) REFERENCES Coupon(Coupon_No)*/
 );
 
 CREATE TABLE Order_list (
   Order_No varchar(5) NOT NULL,
   Game_No varchar(5) NOT NULL
-   /*FOREIGN KEY (Game_No) REFERENCES Game(Game_No)*/
 );
 
 CREATE TABLE User_Comment (
   Comment_No varchar(5) NOT NULL,
-  User_No varchar(5) NOT NULL,
+  Buyer_No varchar(5) NOT NULL,
   Game_No varchar(5) NOT NULL,
   Content varchar(500) NOT NULL,
   PRIMARY KEY(Comment_No)
-  /*FOREIGN KEY (Game_No) REFERENCES Game(Game_No),
-  FOREIGN KEY (User_No) REFERENCES User(User_No)*/
 );
